782c0520b68b496feb5b4c8e4f100e5427819afb
LillyABC <98838491+LillyABC@users.noreply.github.com>
2022-03-08 19:29:18 +0100

    removing comment tags

--- src/javascript.js
+++ src/javascript.js
@@ -22,13 +22,13 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}.`;
 }
 
-/* function getForecast(coordinates) {
+function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "e18f3eef8f69e4c6f8550807956494a5";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
-} */
+}
 
 function displayForecast() {
   let forecastElement = document.querySelector(".forecast");
@@ -81,7 +81,7 @@ function displayTemp(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 
-  //getForecast(response.data.coord);
+  getForecast(response.data.coord);
 }
 
 // city
@@ -127,4 +127,4 @@ let celsiusClick = document.querySelector("#bigC");
 celsiusClick.addEventListener("click", showCelsius);
 
 search("Seoul");
-displayForecast();
+//displayForecast();
