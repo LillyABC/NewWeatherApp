810d9898c841df7f5deced1f7678fc5911004803
LillyABC <98838491+LillyABC@users.noreply.github.com>
2022-03-09 12:55:43 +0100

    little changes

--- src/javascript.js
+++ src/javascript.js
@@ -31,10 +31,10 @@ function formatDay(timestamp) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
+  //console.log(coordinates);
   let apiKey = "e18f3eef8f69e4c6f8550807956494a5";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  //console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
